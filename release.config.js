48cfede2d7befc0ba8c9a9008366ef30da68d3d9
Eduardo Borges <euduardoborges@gmail.com>
2023-07-31 11:36:10 -0300

    fix: fix release configuration file

---
+++ release.config.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
+console.log(`branch: ${branch}`);
+
+module.exports = {
+  baseBranch: 'main',
+  branches: [
+    { name: 'main', prerelease: false },
+    { name: 'develop', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  tagFormat: '${version}',
+};
